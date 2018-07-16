4c3cc0951aac83850e0b9becbffed835949b1770
Ernesto Aguaysol <ernestoaguaysol@gmail.com>
2018-07-16 17:51:19 -0300

    config db con PDO y settings

--- src/settings.php
+++ src/settings.php
@@ -15,5 +15,15 @@ return [
             'path' => isset($_ENV['docker']) ? 'php://stdout' : __DIR__ . '/../logs/app.log',
             'level' => \Monolog\Logger::DEBUG,
         ],
+        // config db
+        'app_token_name' => 'APP_TOKEN',
+        'db' => [
+            'host' => 'localhost',
+            'dbname' => 'misclientes',
+            'user' => 'root',
+            'pass' => '',
+        ]
     ],
+    
+
 ];
